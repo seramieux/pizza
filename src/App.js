f9735f6135c0b99860cc3c2c587d23d975314430
unknown <iskhakabdusalamovg@gmail.com>
2024-10-18 23:57:52 +0500

    home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import "./App.css";
 import Header from "./components/Header";
 import Home from "./pages/Home";
-import "./App.css";
 
 const App = () => {
   return (
-    <div className="font-Nunito max-w-[1380px] my-0 mx-auto bg-[#FFF] shadow-[0_14px_20px_0_rgba(0,0,0,0.05)]">
+    <div className="max-w-[1380px] my-0 mx-auto bg-[#FFF] shadow-App font-Nunito">
       <Header />
       <Home />
     </div>
