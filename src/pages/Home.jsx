9a2107fcd51be5f93392754bc5236cef8273a06d
Iskhak <iskhakabdusalamovg@gmail.com>
2024-10-31 03:42:44 +0500

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,58 @@
-import { useState, useEffect } from "react";
+import React from "react";
+import Context from "../context";
 import axios from "axios";
 import Categories from "../components/Categories";
 import Sort from "../components/Sort";
 import Card from "../components/Card";
+import Skeleton from "../components/Skeleton";
 
 const Home = () => {
-  const [items, setItems] = useState([]);
-  useEffect(() => {
-    const apiUrl = "https://6703a869ab8a8f8927311299.mockapi.io/items";
-    axios.get(apiUrl).then((response) => {
-      setItems(response.data);
-    });
-  }, []);
+  const { searchValue } = React.useContext(Context);
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [sortType, setSortType] = React.useState({
+    name: "популярности (DESC)",
+    sortProperty: "rating",
+  });
+
+  React.useEffect(() => {
+    const apiUrl = "https://6703a869ab8a8f8927311299.mockapi.io/items?";
+    const category = categoryId > 0 ? `category=${categoryId}` : "";
+    const sortBy = sortType.sortProperty.replace("-", "");
+    const order = sortType.sortProperty.includes("-") ? "asc" : "desc";
+    setIsLoading(true);
+    axios
+      .get(`${apiUrl + category}&sortBy=${sortBy}&order=${order}`)
+      .then((response) => {
+        setItems(response.data);
+        setIsLoading(false);
+      });
+  }, [categoryId, sortType]);
 
   return (
     <div className="py-[40px] px-[65px]">
-      <b className="font-extrabold text-[36px] leading-[49px]">Все пиццы</b>
+      <span className="text-[36px] font-extrabold leading-[49px]">
+        Все пиццы
+      </span>
       <div className="flex justify-between items-center mt-[20px]">
-        <Categories />
-        <Sort />
+        <Categories
+          categoryId={categoryId}
+          onChangeCategory={(index) => setCategoryId(index)}
+        />
+        <Sort
+          sortType={sortType}
+          onChangeSort={(index) => setSortType(index)}
+        />
       </div>
       <div className="grid grid-cols-4 gap-[40px]">
-        {items.map((object) => (
-          <Card key={object.id} {...object} />
-        ))}
+        {isLoading
+          ? [...Array(30)].map((_, index) => <Skeleton key={index} />)
+          : items
+              .filter(({ title }) =>
+                title.toLowerCase().includes(searchValue.toLowerCase())
+              )
+              .map((item) => <Card key={item.id} {...item} />)}
       </div>
     </div>
   );
