4a6f5e8d7c396f47c9315476183e6040437c2b1f
Iskhak <iskhakabdusalamovg@gmail.com>
2024-10-31 03:31:13 +0500

    sort

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,40 +1,47 @@
-import { useState } from "react";
+import React from "react";
+import arrows from "../assets/img/arrows.svg";
 
-const Sort = () => {
-  const [open, setOpen] = useState(false);
-  const [selected, setSelected] = useState(0);
-  const list = ["популярности", "цене", "алфавиту"];
+const Sort = ({ sortType, onChangeSort }) => {
+  const [open, setOpen] = React.useState(false);
+  const list = [
+    { name: "популярности (DESC)", sortProperty: "rating" },
+    { name: "популярности (ASC)", sortProperty: "-rating" },
+    { name: "цене (DESC)", sortProperty: "price" },
+    { name: "цене (ASC)", sortProperty: "-price" },
+    { name: "алфавиту (DESC)", sortProperty: "title" },
+    { name: "алфавиту (ASC)", sortProperty: "-title" },
+  ];
 
   const onClickListItem = (index) => {
-    setSelected(index);
+    onChangeSort(index);
     setOpen(false);
   };
 
   return (
-    <div className="relative">
-      <div className="flex items-center bg-[#FAFAFA] py-[17px] px-[22px] rounded-[15px] gap-[7px]">
-        <img className="w-[16px] h-[14px]" src="/arrow.svg" alt="arrow" />
+    <div className="relative z-[1]">
+      <div className="flex items-center bg-[#fafafa] py-[17px] px-[22px] rounded-[15px] gap-[7px]">
+        <img className="w-[16px] h-[14px]" src={arrows} alt="arrows" />
         <b className="text-[16px] leading-[22px]">Сортировка по:</b>
         <b
           onClick={() => setOpen(!open)}
-          className="text-[16px] leading-[22px] text-[#FE5F00] cursor-pointer"
+          className="text-[16px] leading-[22px] text-[#fe5f00] cursor-pointer"
         >
-          {list[selected]}
+          {sortType.name}
         </b>
       </div>
       {open && (
-        <ul className="absolute rounded-[10px] right-0 bg-[#FFFFFF] py-[10px] cursor-pointer select-none mt-[5px]">
-          {list.map((value, index) => (
+        <ul className="rounded-[10px] right-0 bg-[#FFf] py-[10px] px-0 cursor-pointer select-none mt-[5px] absolute z-[1]">
+          {list.map((object, index) => (
             <li
               key={index}
-              onClick={() => onClickListItem(index)}
+              onClick={() => onClickListItem(object)}
               className={
-                selected === index
-                  ? "font-bold text-[16px] leading-[17px] rounded-[10px] tracking-[1.5%] py-[10px] text-[#FE5F1E] px-[17px] bg-[#FE5F1E0D]"
-                  : "text-[16px] leading-[17px] rounded-[10px] tracking-[1.5%] py-[10px] px-[17px] hover:bg-[#FE5F1E0D]"
+                sortType.sortProperty === object.sortProperty
+                  ? "text-[16px] leading-[17px] rounded-[10px] tracking-[1.5%] py-[10px] px-[17px] bg-[#fe5f1e0d] text-[#fe5f1e] font-bold hover:bg-[#fe5f1e0d]"
+                  : "text-[16px] leading-[17px] rounded-[10px] tracking-[1.5%] py-[10px] px-[17px] hover:bg-[#fe5f1e0d]"
               }
             >
-              {value}
+              {object.name}
             </li>
           ))}
         </ul>
